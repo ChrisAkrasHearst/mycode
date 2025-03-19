95827aa2463a844818569a0ab462f25380913ad3
ChrisAkrasHearst <chris.akras@hearst.com>
2025-03-19 15:46:31 +0000

    fun with flags

---
+++ arguments01.go
@@ -0,0 +1,51 @@
+/* Alta3 Research | RZFeeser
+   Reading positional arguments from the command line*/
+   
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+
+    // os.Args provides access to raw CLI arguments
+    argsWithProg := os.Args         // includes program name
+    
+    argsWithoutProg := os.Args[1:]  // does not include program name
+
+    arg := os.Args[3]               // select the 3rd argument
+                                    // remember: the 1st argument is the name
+                                    //           of the program
+
+    last_arg := os.Args[len(os.Args)-1]         // select the last argument         // select the last argument
+
+    // display the results
+    fmt.Println("All arguments, including the program name:")
+    fmt.Println(argsWithProg)
+    
+    fmt.Println("All arguments, (without the program name:")
+    fmt.Println(argsWithoutProg)
+    
+    // select "just" the 3rd argument
+    fmt.Println(arg)
+    
+    // display the last argument
+    fmt.Println(last_arg)
+
+    // use the len() function to return length
+    argLength := len(os.Args[1:])
+
+    // use fmt.Printf() to format string
+    fmt.Printf("Arg length is %d\n", argLength)
+
+    // this is called a "range" loop
+    // i is an integer starting at 0
+    // a is the string (argument in this case) corresponding to the integer
+    for i, a := range os.Args[1:] {                 // loop across args
+        fmt.Printf("Arg %d is %s\n", i+1, a)        // use a format string
+    }
+
+}
+
